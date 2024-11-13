dd4a2002c9fcd79e710ecaa9166fa5a6aaaf2220
Lorena Martinez <merchanlorena30@gmail.com>
2024-11-13 18:52:05 -0500

    creación del proyecto

---
+++ src/main/java/com/uptc/frw/ConsecionarioDockerApplication.java
@@ -0,0 +1,13 @@
+package com.uptc.frw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsecionarioDockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsecionarioDockerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uptc/frw/ConsecionarioDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uptc.frw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsecionarioDockerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

caace24f950c4c917f122330a746e0fc9c135a64
Lorena Martinez <merchanlorena30@gmail.com>
2024-11-13 20:13:14 -0500

    Mapeado de la entidad Opciones Modelo

---
+++ src/main/java/com/uptc/frw/jpa/models/OpcionesModelo.java
@@ -0,0 +1,55 @@
+package com.uptc.frw.jpa.models;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "opciones_modelo")
+public class OpcionesModelo {
+    @Id
+    @Column(name = "ID_OPCIONES_MODELO")
+    private Integer id;
+
+    @Column(name = "NOMBRE")
+    private String nombre;
+
+    @Column(name = "DESCRIPCION")
+    private String descripcion;
+
+
+    @OneToMany(mappedBy ="opcionesmodelo" )
+    private List<DetalleCompra> detalleOpcionesModelo;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public List<DetalleCompra> getDetalleOpcionesModelo() {
+        return detalleOpcionesModelo;
+    }
+
+    public void setDetalleOpcionesModelo(List<DetalleCompra> detalleOpcionesModelo) {
+        this.detalleOpcionesModelo = detalleOpcionesModelo;
+    }
+}

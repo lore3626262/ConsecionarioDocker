471bad3329a64673fa561c81c6f918bf5650590c
Lorena Martinez <merchanlorena30@gmail.com>
2024-11-13 20:14:31 -0500

    Mapeado de la entidad Cesion

---
+++ src/main/java/com/uptc/frw/jpa/models/Cesion.java
@@ -0,0 +1,91 @@
+package com.uptc.frw.jpa.models;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Date;
+
+@Entity
+@Table(name = "cesion")
+public class Cesion {
+    @Id
+    @Column(name = "ID_CESION")
+    private int id;
+
+    @Column(name = "PLACA",insertable = false, updatable = false)
+    private String placa;
+
+    @Column(name = "NUMERO_FACTURA",insertable = false, updatable = false)
+    private int numeroFactura;
+
+    @Column(name = "TASACION")
+    private double tasacion;
+
+    @Column(name = "FECHA_CESION")
+    private Date fechaCesion;
+
+    @ManyToOne
+    @JoinColumn(name = "PLACA")
+    private Vehiculo cesionVehiculo;
+
+    @ManyToOne
+    @JoinColumn(name = "NUMERO_FACTURA")
+    private Compra cesionCompra;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public int getNumeroFactura() {
+        return numeroFactura;
+    }
+
+    public void setNumeroFactura(int numeroFactura) {
+        this.numeroFactura = numeroFactura;
+    }
+
+    public double getTasacion() {
+        return tasacion;
+    }
+
+    public void setTasacion(double tasacion) {
+        this.tasacion = tasacion;
+    }
+
+    public Date getFechaCesion() {
+        return fechaCesion;
+    }
+
+    public void setFechaCesion(Date fechaCesion) {
+        this.fechaCesion = fechaCesion;
+    }
+
+    public Vehiculo getCesionVehiculo() {
+        return cesionVehiculo;
+    }
+
+    public void setCesionVehiculo(Vehiculo cesionVehiculo) {
+        this.cesionVehiculo = cesionVehiculo;
+    }
+
+    public Compra getCesionCompra() {
+        return cesionCompra;
+    }
+
+    public void setCesionCompra(Compra cesionCompra) {
+        this.cesionCompra = cesionCompra;
+    }
+}
